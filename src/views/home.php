74cf5ffd481918281a90a832d0fe0e84fb7fc9bc
Martin Wilches <martindotdev@gmail.com>
2025-08-17 21:02:44 -0500

    Estilos home

--- src/views/home.php
+++ src/views/home.php
@@ -9,17 +9,21 @@ $notes = Note::getAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/views/resources/main.css">
     <title>Home</title>
 </head>
 <body>
     <h1>Home</h1>
 
-    <?php foreach($notes as $note): ?>
-        <a href="?view=view&uuid=<?php echo $note->getUuid(); ?>">
-            <div class="note-preview">
-                <div class="title"><?php echo $note->getTitle(); ?></div>
-            </div>
-        </a>
-    <?php endforeach; ?>
+    <div class="home-card__container">
+        <?php foreach($notes as $note): ?>
+            <a href="?view=view&uuid=<?php echo $note->getUuid(); ?>" class="home-card">
+                <div class="note-preview">
+                    <h2 class="title"><?php echo $note->getTitle(); ?></h2>
+                    <p class="content"><?php echo $note->getContent(); ?></p>
+                </div>
+            </a>
+        <?php endforeach; ?>
+    </div>
 </body>
 </html>
